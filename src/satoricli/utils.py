7c541bcb16663e27f5ddc1da052d6d54e3995ae7
Juaniimedina7 <84475845+Juaniimedina7@users.noreply.github.com>
2024-10-14 11:22:13 -0300

    Update utils.py

--- src/satoricli/utils.py
+++ src/satoricli/utils.py
@@ -1,15 +1,17 @@
 from pathlib import Path
-
 import yaml
+import argparse
 
-
-def load_config():
+def load_config(config_path=None):
     config = {}
 
-    locations = (
+    locations = [
         Path(".satori_credentials.yml"),
         Path.home() / ".satori_credentials.yml",
-    )
+    ]
+
+    if config_path:
+        locations.insert(0, Path(config_path))
 
     for location in locations:
         if not location.is_file():
@@ -19,7 +21,15 @@ def load_config():
 
     return config
 
-
 def save_config(config: dict):  # TODO: Global/local config
     with (Path.home() / ".satori_credentials.yml").open("w") as f:
         f.write(yaml.safe_dump(config))
+
+def main():
+    export_arg = argparse.ArgumentParser(description="Satori CLI")
+    export_arg.add_argument("--config", type=str, help="Path to the configuration file")
+
+
+
+if __name__ == "__main__":
+    main()
